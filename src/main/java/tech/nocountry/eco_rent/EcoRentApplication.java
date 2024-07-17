b38c94d1fece1a6515861c274182e74ed5e1a058
GON-CAB-8 <cabralmartin8@gmail.com>
2024-07-17 15:55:17 -0300

    creado los directorios e inicializacion de mvn

---
+++ src/main/java/tech/nocountry/eco_rent/EcoRentApplication.java
@@ -0,0 +1,13 @@
+package tech.nocountry.eco_rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/nocountry/eco_rent/EcoRentApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nocountry.eco_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
